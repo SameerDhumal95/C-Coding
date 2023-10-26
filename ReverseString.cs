5a11c4e63922fd39adbccf110622f4fcd32e5136
Sameer <106860465+SameerDhumal95@users.noreply.github.com>
2023-10-26 19:49:18 +0530

    Add files via upload

---
+++ ReverseString.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Coding
+{
+    public class ReverseString
+    {
+        public static void Main(string[] args)
+        {
+            string str = "Hello dot net";
+            string []sstr = str.Split(" ");
+
+            for (int i = sstr.Length - 1; i >= 0; i--)
+            { 
+                Console.Write(sstr[i]+" "); 
+            }
+            
+        }
+    }
+}
