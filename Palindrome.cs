1e960a0f88d5088c980c803360f19b3897cb7df2
Sameer <106860465+SameerDhumal95@users.noreply.github.com>
2023-10-26 17:53:45 +0530

    Add files via upload

---
+++ Palindrome.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Coding
+{
+    public class Palindrome
+    {
+        public static void Main(string[] args)
+        {
+            string str = "Naman".ToUpper();
+            string rstr = "";
+
+            for(int i= str.Length-1; i>=0; i--)
+            {
+                rstr = rstr + str[i];
+            }
+
+            if(rstr==str)
+            {
+                Console.WriteLine("Palindrom");
+            }
+            else
+            {
+                Console.WriteLine("Not Palindrome");
+            }
+
+        }
+    }
+}
